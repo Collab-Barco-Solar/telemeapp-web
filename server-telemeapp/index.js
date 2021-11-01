7efff05be4aa7cc5d0362de090c359782f1a5d24
andreocunha <andreoliveiracunha20@gmail.com>
2021-11-01 15:11:47 -0300

    server telemeapp

--- server-telemeapp/index.js
+++ server-telemeapp/index.js
@@ -5,7 +5,21 @@ const io = require('socket.io')(4000, {
 });
 
 let dados = '20.000,10.000'
-let vetorDados = ['20.000,10.000','20.000,10.000','20.000,10.000']
+
+//String(current_mppt) + "," + String(current_alimentation) + "," + String(voltage_batteries) + "," + String(current_motor) + "," + String(temperature) + "," + String(humidity) + "," + String(voltage_alimentation) + "," + gps;
+
+
+let vetorDados = []
+
+
+function saveDataVector(data){
+    if(vetorDados.length > 20000){
+        vetorDados.shift()
+    }
+    vetorDados.push(data)
+    // console.log("entrou aqui")
+}
+
 
 io.on("connection", socket => {
     console.log("USUARIO: " + socket.id);
@@ -13,8 +27,23 @@ io.on("connection", socket => {
     socket.emit("info", vetorDados); // emite apenas para quem se conectou no momento
 
     socket.on("newinfo", (data) => {
-        console.log(data);
-        dados = data;
-        io.emit("info", data); // emite para todos
+        // console.log(data);
+        let arrayDados = data.split(',');
+        dados = {
+            current_mppt: arrayDados[0],
+            current_alimentation: arrayDados[1],
+            voltage_batteries: arrayDados[2],
+            current_motor: arrayDados[3],
+            temperature: arrayDados[4],
+            humidity: arrayDados[5],
+            voltage_alimentation: arrayDados[6],
+            lat: arrayDados[7],
+            long: arrayDados[8],
+            speed: arrayDados[9]
+        }
+        console.log(dados);
+        
+        saveDataVector(dados)
+        io.emit("info", dados); // emite para todos
     })
 })
